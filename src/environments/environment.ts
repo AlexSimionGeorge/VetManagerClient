8b85973a97f96bea50392f6cd740eb3da75dc499
AlexSimionGeorge <alexsimiongeorge@gmail.com>
2025-06-19 14:55:09 +0200

    excluded environment file

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8000/api',
-  googleClientId: '125293043339-4pdfmaodjbla41812n6vslj4te6fb7is.apps.googleusercontent.com',
-};
